799c88be8079ee7591bb0b11a085638ee327f222
Fabio🐸 <diconza57@hotmail.it>
2026-09-19 18:31:30 +0200

    Remove renamed provider file IStorageSourceProvider.cs

--- SamplePlugin/Core/Inventory/Providers/IStorageSourceProvider.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace FROG.Core.Inventory.Providers;
-
-public interface IStorageSourceProvider
-{
-    IReadOnlyList<InventorySource> GetSourcesForCharacter(
-        ulong characterId);
-}
